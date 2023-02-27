8da4f215d3752175dfe60166eb46d5b9ec59e052
Daniele Frasca <daniele.frasca@seven.one>
2023-02-27 10:58:54 +0100

    upgrade libs

--- src/utils/api_helper.rs
+++ src/utils/api_helper.rs
@@ -1,9 +1,13 @@
-use lambda_http::{Response, http::StatusCode};
+use lambda_http::{http::StatusCode, Response};
 
 pub struct ApiHelper;
 
 impl ApiHelper {
-    pub fn response(status_code: StatusCode, body: String, content_type: String) -> Response<String> {
+    pub fn response(
+        status_code: StatusCode,
+        body: String,
+        content_type: String,
+    ) -> Response<String> {
         Response::builder()
             .status(status_code)
             .header("Content-Type", content_type)
